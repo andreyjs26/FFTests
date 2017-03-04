63b8f2ade420298d4f60f911d7ac531fb455ca46
jaensoli <angel-andrey.jaen-solis@hp.com>
2017-03-03 22:31:22 -0600

    first test

---
+++ VendorFactory/VendorLibrary.Tests/Controllers/VendorControllerTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorLibrary;
+using VendorLibrary.Controllers;
+
+namespace VendorLibrary.Tests.Controllers
+{
+    [TestClass]
+    public class VendorControllerTest
+    {
+        [TestMethod]
+        public void test1()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            int result = controller.getSoldQuantity("1/31/2017");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(320, result);                  
+        }
+
+        [TestMethod]
+        public void test2()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            int result = controller.getSoldQuantity("1/25/2017");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result);
+        }
+
+
+        [TestMethod]
+        public void test3()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            int result = controller.getSoldQuantityPerItem("1/31/2017","chips");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(110, result);
+        }
+
+
+        [TestMethod]
+        public void test4()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            int result = controller.getSoldQuantityPerItem("1/29/2017", "cola soda");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result);
+        }
+        
+
+        [TestMethod]
+        public void test5()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            List<string> result = controller.getMostSoldItems("1/31/2017");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+        }
+
+
+        [TestMethod]
+        public void test6()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            List<string> result = controller.getMostSoldItems("1/27/2017");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count);
+        }
+
+
+        [TestMethod]
+        public void test7()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            List<string> result = controller.getMostSoldDates("cola soda");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void test8()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            List<string> result = controller.getMostSoldDates("cream cookie");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void test9()
+        {
+            // Arrange
+            VendorController controller = new VendorController();
+
+            // Act
+            List<string> result = controller.getMostSoldDates("chocolate");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count);
+        }
+
+
+    }
+}
---
+++ VendorFactory/VendorLibrary/Controllers/VendorController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace VendorLibrary.Controllers
+{
+    public class VendorController : ApiController
+    {
+
+        //This function returns the report formatted in rows, the reason of the function is beacuse as the tool is going to be consulted thousands of times, we need a way that don't affect users running at the same specific time.
+        //And returning  and instancing the result each time should  work for multiple users at the same time .
+        private List<string> returnReport() 
+        {            
+            string report = "1/31/2017,cola soda,100;1/31/2017,cream cookie,60;1/31/2017,cream cookie,50;1/31/2017,chips,110;1/29/2017,cream cookie,130;1/28/2017,cola soda,100";
+
+            List<string> rows = report.Split(';').Select(sValue => sValue.Trim()).ToList();
+
+            return rows;
+
+        }
+
+        // GET api/vendor/getSoldQuantity?date={date}
+        public int getSoldQuantity(string date)
+        {
+            
+            List<string> formatReport = returnReport();
+
+            var result = (
+                            (from x in formatReport
+                            where x.Contains(date)
+                            select new
+                            {
+                                number =Int32.Parse(x.Split(',')[2])
+                            }).ToList()
+                         ).Sum(x => x.number);
+
+            return result;           
+        }
+
+        // GET api/vendor/getSoldQuantityPerItem?date={date}&itemName={itemName}
+        public int getSoldQuantityPerItem(string date, string itemName)
+        {
+            List<string> formatReport = returnReport();
+
+            var result = (
+                            (from x in formatReport
+                             where x.Contains(date) && x.Contains(itemName)
+                             select new
+                             {
+                                 number = Int32.Parse(x.Split(',')[2])
+                             }).ToList()
+                         ).Sum(x => x.number);
+
+            return result;           
+        }
+
+        // GET api/vendor/getMostSoldItems?date={date}
+        public List<string> getMostSoldItems(string date)
+        {
+            List<string> formatReport = returnReport();
+
+            var result =    (from x in formatReport
+                             where x.Contains(date)                           
+                            select new
+                             {
+                                 itemName = x.Split(',')[1],
+                                 number = x.Split(',')[2]
+                             }).ToList();
+
+            var grouped = (from x in result
+                           group x by new { x.itemName } into g                          
+                           select new { g.Key.itemName, sum = g.Sum(x => Int32.Parse(x.number)) }
+                          ).ToList();
+
+            return grouped.Where(x => x.sum == grouped.Max(y => y.sum)).Select(x => x.itemName).ToList();
+                                    
+        }
+
+        // GET api/vendor/getMostSoldDates?itemName={itemName}
+        public List<string> getMostSoldDates(string itemName)
+        {
+            List<string> formatReport = returnReport();
+            var result =     (from x in formatReport
+                              where x.Contains(itemName)
+                             select new
+                             {
+                                 date = x.Split(',')[0],
+                                 number = x.Split(',')[2]
+                             }).ToList();
+
+            var grouped = (from x in result
+                           group x by new { x.date } into g
+                           select new { g.Key.date, sum = g.Sum(x => Int32.Parse(x.number)) }
+                          ).ToList();
+
+            return grouped.Where(x => x.sum == grouped.Max(y => y.sum)).Select(x => x.date).ToList();          
+        }
+    }
+}
